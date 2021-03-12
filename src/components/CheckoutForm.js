b7ea01815e0796120930160faa8a945e6c9f01c9
Jonathan1600 <70250506+Jonathan1600@users.noreply.github.com>
2021-03-12 14:11:35 -0700

    mvp done

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -15,11 +16,7 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
-
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  const [values, handleChanges] = useForm(initialValue);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -33,6 +30,7 @@ const CheckoutForm = (props) => {
         <label>
           First Name:
           <input
+            data-testid="firstName"
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
@@ -41,6 +39,7 @@ const CheckoutForm = (props) => {
         <label>
           Last Name:
           <input
+            data-testid="lastName"
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
@@ -49,6 +48,7 @@ const CheckoutForm = (props) => {
         <label>
           Address:
           <input
+            data-testid="adress"
             name="address"
             value={values.address}
             onChange={handleChanges}
@@ -56,15 +56,30 @@ const CheckoutForm = (props) => {
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input
+            data-testid="city"
+            name="city"
+            value={values.city}
+            onChange={handleChanges}
+          />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input
+            data-testid="state"
+            name="state"
+            value={values.state}
+            onChange={handleChanges}
+          />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input
+            data-testid="zip"
+            name="zip"
+            value={values.zip}
+            onChange={handleChanges}
+          />
         </label>
         <button>Checkout</button>
       </form>
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,31 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, component } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
-
+import userEvent from "@testing-library/user-event";
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
+  const header = screen.queryByText("Checkout Form");
+  expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+  render(<CheckoutForm />);
+  const firstInput = screen.queryByLabelText("First Name:");
+  const lastInput = screen.queryByLabelText("Last Name:");
+  const adressInput = screen.queryByLabelText("Address:");
+  const cityInput = screen.queryByLabelText("City:");
+  const stateInput = screen.queryByLabelText("State:");
+  const zipInput = screen.queryByLabelText("Zip:");
+  const submitButton = screen.queryByRole("button");
+  userEvent.type(firstInput, "Jonathan");
+  userEvent.type(lastInput, "Calderon");
+  userEvent.type(adressInput, "1000 N Street");
+  userEvent.type(cityInput, "San City");
+  userEvent.type(stateInput, "Arizona");
+  userEvent.type(zipInput, "10101");
+  userEvent.click(submitButton);
+  const successMessage = await screen.queryByTestId("successMessage");
+  expect(successMessage).toBeInTheDocument();
+});
